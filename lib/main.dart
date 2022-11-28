68442575f957d123e79dc3eb4e6e1965697b81cb
BabyTigerDaddy <babytiggerdaddy@gmail.com>
2022-11-29 01:14:42 +0900

    Create Main 2 TabView 1. Add material_screen.dart & cupertino_screen.dart 2. Make ListView in each screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:baby_tiger_sample/screens/main_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
+      home: MainScreen(),
     );
   }
 }
